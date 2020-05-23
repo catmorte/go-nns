aae361359353e7b0c97cd0c2296ed7b5ddc48626
Ram Su <catmorte@gmail.com>
2020-05-23 23:36:38 +0300

    remove bad

--- example/cmd/ffnn/single_result/main.go
+++ example/cmd/ffnn/single_result/main.go
@@ -19,7 +19,7 @@ func main() {
 	net.AddLayer(2, weightGen, activationCalc, 0.01)
 	net.AddLayer(1, weightGen, activationCalc, 0.01)
 
-	for i := 0; i < 200000; i++ {
+	for i := 0; i < 400000; i++ {
 		net.Train([]float64{1, 1}, []float64{0})
 		net.Train([]float64{1, 0}, []float64{1})
 		net.Train([]float64{0, 1}, []float64{1})
